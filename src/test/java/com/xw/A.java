479c34a68718a428c8e2c09b136a0523c5e0905e
hhlxw <1013620309@qq.com>
2021-04-23 11:29:37 +0800

    创建分支1

--- src/test/java/com/xw/A.java
+++ src/test/java/com/xw/A.java
@@ -2,7 +2,15 @@ package com.xw;
 
 public class A {
    private String name;
-
+/**
+ *@Description:
+ *
+ * @param
+ * @return java.lang.String
+ * @throws
+ * @author XiongWei
+ * @date 2021/4/23 11:29
+*/
     public String getName() {
         return name;
     }

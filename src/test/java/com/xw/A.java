ec18974c4fb8ea615d100febbc5ddc100f785623
hhlxw <1013620309@qq.com>
2021-04-23 15:16:45 +0800

    分支1push

--- src/test/java/com/xw/A.java
+++ src/test/java/com/xw/A.java
@@ -14,7 +14,15 @@ public class A {
     public String getName() {
         return name;
     }
-
+/**
+ *@Description:
+ *
+ * @param name
+ * @return void
+ * @throws
+ * @author XiongWei
+ * @date 2021/4/23 15:12
+*/
     public void setName(String name) {
         this.name = name;
     }

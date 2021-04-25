0485b8af3aec7cf22d066c5667937e9b0bddadc3
hhlxw <1013620309@qq.com>
2021-04-25 17:30:37 +0800

    分支1push5

--- src/main/java/com/xw/Test1.java
+++ src/main/java/com/xw/Test1.java
@@ -6,6 +6,15 @@ package com.xw;
  * @date 2021/4/25
  */
 public class Test1 {
+    /**
+     *@Description:
+     *
+     * @param args
+     * @return void
+     * @throws
+     * @author XiongWei
+     * @date 2021/4/25 17:30
+    */
     public static void main(String[] args) {
         int a = 0;//00000000
         char b= '0';

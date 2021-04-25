04fd070a3d29ee1eada84a85155cd71c57288cb9
hhlxw <1013620309@qq.com>
2021-04-25 17:21:07 +0800

    分支1push4

---
+++ src/main/java/com/xw/Test1.java
@@ -0,0 +1,15 @@
+package com.xw;
+
+/**
+ * @Description:
+ * @author: XiongWei
+ * @date 2021/4/25
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        int a = 0;//00000000
+        char b= '0';
+        System.out.println(Integer.toBinaryString(a));//00110000
+
+    }
+}

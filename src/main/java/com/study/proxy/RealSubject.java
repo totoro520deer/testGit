1922f4b0197c45691f9b8f798199755fa9ac108e
hhlxw <1013620309@qq.com>
2021-06-18 17:24:37 +0800

    为了让.gitignore生效

--- src/main/java/com/study/proxy/RealSubject.java
+++
@@ -1,9 +0,0 @@
-package com.study.proxy;
-
-//真实角色：实现了Subject的request()方法
-public class RealSubject implements Subject{
-
-    public void request(){
-        System.out.println("From real subject.");
-    }
-}
--- src/main/java/com/study/proxy/Subject.java
+++
@@ -1,5 +0,0 @@
-package com.study.proxy;
-//抽象角色（JDK动态代理只能代理接口）
-public interface Subject {
-    public void request();
-}
